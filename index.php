a8e6b9d223175e9942817475901d8134271828ec
DomenicoLucarelli <domyluca@gmail.com>
2023-04-18 14:49:51 +0200

    feat: aggiunta tabella

--- index.php
+++ index.php
@@ -49,9 +49,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <?php 
+    <!-- <?php 
     
     foreach($hotels as  $hotel){
         foreach ($hotel as $key => $element) {
@@ -62,6 +64,55 @@
         echo '<br>';
     };
     
+    ?> -->
+
+<table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to center</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <?php 
+    
+    foreach($hotels as  $hotel){
+    
     ?>
+        
+        <tr>
+           <?php
+           
+           foreach ($hotel as $key => $element) {
+            
+            ?>
+            
+            <td> <?php echo $element ?> </td>
+
+        <?php
+
+        };
+
+        ?>
+
+        </tr>
+
+    <?php
+
+    };
+
+    ?>
+  </tbody>
+</table>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
